efb715190f7bd0e82d5f2fb6e9a416d6df70060c
Katherine Meine <Katherine Meine>
2016-10-12 13:37:11 +0900

    java files reupload

---
+++ Player.java
@@ -0,0 +1,18 @@
+class Player {
+  private String name = null;
+  private int number = 0;
+  public Player(String name) {
+    this.name = name;
+  }
+  public void play() {
+    Dice d = new Dice();
+    System.out.print("Player " + this.name + "'s turn: ");
+    number = d.rollDice();
+  }
+  public int getNum() {
+	return this.number;
+  }
+  public String getName() {
+	return this.name;
+  }
+}
---
+++ Subway.java
@@ -0,0 +1,9 @@
+﻿class Subway {
+  int line;
+  void run() {
+    System.out.println(line+" line runs");
+  }
+  void setLine(int l) {
+    line = l;
+  }
+}
