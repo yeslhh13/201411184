f136e075be2b5cc1d6923d79a993c0a5eb315b32
Katherine Meine <Katherine Meine>
2016-10-12 13:28:38 +0900

    ignore

---
+++ Hello.java
@@ -0,0 +1,9 @@
+﻿class Hello {
+  String name;
+  Hello(String name) {
+    this.name = name;
+  }
+  void sayHello() {
+    System.out.println("Hello "+name);
+  }
+}
---
+++ Person.java
@@ -0,0 +1,12 @@
+class Person {
+	private float weight;
+	public Person(float w) {
+		setWeight(w);
+	}
+	public void setWeight(float w) {
+		this.weight = w;
+	}
+	private float getWeight() {
+		return weight;
+	}
+}
