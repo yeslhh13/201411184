7b49ff61bd66dc879a299a60bfd4d31f658a18ce
Katherine Meine <Katherine Meine>
2016-11-25 17:42:50 +0900

    select

--- src/com/sd/inhrt/SelectMain.java
+++ src/com/sd/inhrt/SelectMain.java
@@ -3,8 +3,15 @@ package com.sd.inhrt;
 public class SelectMain {
 	public static void main(String[] args) {
 		Selection s = new Selection();
-		Rectangle r = new Rectangle(20, 30);
+		Rectangle r = new Rectangle(20.0, 30.0);
+		Rectangle c = new Cube(10.0, 20.0, 30.0);
+		
 		s.select(r);
+		s.select(c);
+		
 		r.getSelected();
+		c.getSelected();
+		
+		s.print();
 	}
 }
--- src/com/sd/inhrt/Selection.java
+++ src/com/sd/inhrt/Selection.java
@@ -1,10 +1,18 @@
 package com.sd.inhrt;
 
+import java.util.ArrayList;
 public class Selection {
 	Selection() {};
+	ArrayList<Rectangle> sArr = new ArrayList<Rectangle>();
 	
 	public void select(Rectangle r) {
 		r.setSelected(true);
-		System.out.println("selected: " + r.isSelected);
+		sArr.add(r);
+		System.out.println(r.getClass() + " - selected: " + r.isSelected);
+	}
+	
+	public void print() {
+		for (Rectangle r : sArr)
+			System.out.println(r.toString());
 	}
 }
